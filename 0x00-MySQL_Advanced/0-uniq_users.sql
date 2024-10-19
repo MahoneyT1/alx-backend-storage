b0972cd35c9235166f642e1ce9ad6359a4ec3cb3
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-19 18:06:40 +0100

    created a user table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -8,8 +8,8 @@
 -- Your script can be executed on any database
 
 
-CREATE TABLE IF NOT EXISTS users(
-    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE users(
+    id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
