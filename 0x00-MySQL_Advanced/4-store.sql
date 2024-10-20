7dabcd45d4abace6d0be43b81722564ed1f200fc
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-20 14:02:42 +0100

    a trigger that deploy on insert

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -7,9 +7,8 @@
 -- can generate issue: network disconnection, crash, etc… to keep your data
 -- in a good shape, let MySQL do it for you!
 
-DELIMITER $$
 CREATE trigger decreasesQuantity
 AFTER INSERT ON orders
     UPDATE items
     SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name
+    WHERE name = NEW.item_name;
