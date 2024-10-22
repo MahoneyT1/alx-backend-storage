ee4a20405f7be442b2cf241d7d0c468e984c13f2
MahoneyT1 <ifeanyitech15@gmail.com>
2024-10-22 05:32:31 +0100

    inserting in mongodb

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,6 +7,7 @@ Prototype: def insert_school(mongo_collection, **kwargs):
 mongo_collection will be the pymongo collection object
 Returns the new _id
 """
+import pymongo
 
 
 def insert_school(mongo_collection, **kwargs):
@@ -18,6 +19,5 @@ def insert_school(mongo_collection, **kwargs):
     Returns the new _id
     """
 
-    new_doc = mongo_collection.insertOne(**kwargs)
-
-    return new_doc._id
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
